60aa4d230c4365660921d8aabe203827e4884389
kallapirankp <kallapiran@knights.ucf.edu>
2017-03-04 13:19:50 -0500

    bug fixes in ItemRequest and ItemSize

--- src/components/ItemRequest.js
+++ src/components/ItemRequest.js
@@ -1,30 +1,49 @@
 import React from "react";
 
+const items = ["Botique","Baby Items", "Others"];
+
 export default class ItemRequest extends React.Component{
     constructor(){
         super();
+        console.log(items);
     }
+    
+    componentWillMount(){
+        this.selectedItems = new Set();
+    }
+
+    itemChecked(itmselectd){
+        if(this.selectedItems.has(itmselectd)){
+            this.selectedItems.delete(itmselectd);
+        }else{
+            this.selectedItems.add(itmselectd);
+        }
+        console.dir(this.selectedItems); 
+
+    }
+
     render(){
         return(
             <div className="category">
                 <div className="itemRequest">
-                    <div className="singleItem">
-                        <input type="checkbox" id="ckbx1" value="Botique" />
-                        <label htmlFor="ckbx1">Botique</label>
-                    </div>
-                    <div className="singleItem">
-                        <input type="checkbox" id="ckbx1" value="Botique" />
-                        <label htmlFor="ckbx1">Baby Items</label>
-                    </div>
-                    <div className="singleItem">
-                        <input type="checkbox" id="ckbx1" value="Botique" />
-                        <label htmlFor="ckbx1">Others</label>
-                    </div>
-
+                    {items.map((item)=> <Checkbox label={item} key={item} toggleCheckBox={() => this.itemChecked(item)} />)}
+                    
                 </div>
-                <div className="itemRequestBtn disableReq">
+                <div className="itemRequestBtn">
                     Request
                 </div>
+            </div>);
+    }
+}
+
+class Checkbox extends React.Component{
+
+
+    render(){
+        return(
+            <div className="singleItem">
+                <input type="checkbox" id={this.props.label} value={this.props.label} onChange={this.props.toggleCheckBox} />
+                <label htmlFor={this.props.label}>{this.props.label}</label>
             </div>);
     }
 }
--- src/components/ItemSize.js
+++ src/components/ItemSize.js
@@ -2,43 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom";
 import $ from "jquery";
 
+const size = ["XS","S","M","L","XL","2XL","None"];
+
 export default class ItemSize extends React.Component{
     constructor(){
         super();
         this.state = {
-            size : null,
-            isSelected : false
-        }
+            size : ""
+        }  
     }
 
-    updateSize(sizeSelected, value){
+    updateSize(sizeSelected){
         this.setState({
-            size : sizeSelected,
-            isSelected : true
+            size : sizeSelected
         });
+        console.log(this.state.size)
     }
 
 
     render(){
         return(
             <div className="itemSize">
-                <Button selected={this.updateSize.bind(this,"XS",1)}>XS</Button>
-                <Button selected={this.updateSize.bind(this,"S",2)}>S</Button>
-                <Button selected={this.updateSize.bind(this,"M",3)}>M</Button>
-                <Button selected={this.updateSize.bind(this,"L",4)}>L</Button>
-                <Button selected={this.updateSize.bind(this,"XL",5)}>XL</Button>
-                <Button selected={this.updateSize.bind(this,"2XL",6)}>2XL</Button>
-                <Button selected={this.updateSize.bind(this,"None",7)}>None</Button>
+                {size.map((value) => <Button selectingSize = {() => this.updateSize(value)} key={value} sizeValue={value}/>)}
             </div>
             );
     }
 }
 
 class Button extends React.Component{
+
+    selected(){
+        $(".itemSize button").css({
+            "background-color": "white",
+            "color": "#609dff"
+            });
+        console.log("it enters here");
+        this.className = "buttonFor"+this.props.sizeValue;
+        $("."+this.className).css({
+            "background-color": "#609dff",
+            "color": "white"
+            });
+        this.props.selectingSize;
+    }
     
     render(){
         return(
-            <button onClick={this.selected}>{this.props.children}</button>
+            <button className={"buttonFor"+this.props.sizeValue} onClick={() => this.selected()}>{this.props.sizeValue}</button>
              );
     }
 }
