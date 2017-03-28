db97436ac70c248500d0d91689f29fc465a046bf
kallis98 <kallapiran@knights.ucf.edu>
2017-03-28 15:12:58 -0400

    update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react"
 import FacebookLoginButton from "./FacebookLoginButton"
 import $ from "jquery"
+import { Link } from "react-router"
+import Auth from "../server/auth/authUserCheck"
 
 export default class Header extends React.Component {
 
@@ -19,6 +21,7 @@ export default class Header extends React.Component {
 
 		this.userInfo = this.userInfo.bind(this)
 		this.sendDetailToParent = this.sendDetailToParent.bind(this)
+
 	}
 
 	userInfo = (userType, username, email, age, photo) => {
@@ -51,42 +54,101 @@ export default class Header extends React.Component {
 
 	checkForButton = () => {
 
-			if(this.state.details.username == "" || this.state.details.username == null){
+			if(Auth.isUserAuthenticated()){
+				$('.dropdown').css({
+					'display' : 'none'
+				})
+
+			}else if(this.state.details.username == "" || this.state.details.username == null){
 				$('.dropdown-content').css({
 					'display' : 'inline-block',
 					'background-color' : 'white',
 					'box-shadow' : '0px 0px 0px 0px'
-				});
-				console.log("why this is happening" + this.state.details.username)
+				})
+				
 			}else{
 				$('.dropdown-content').css({
 					'display' : 'none',
 					'box-shadow' : '0px 8px 16px 0px rgba(0,0,0,0.2)',
 					'background-color' : '#f9f9f9'
-				});
-				console.log("it also enters this")
+				})
 			}
 	}
 
-	render() {
-			return(
-				<div className="header">
-					{this.checkForButton()}
-					<h2 className="title-text"> Consumable </h2>
-					{this.state.details.username == "" || this.state.details.username == null ? 
-					<h4 className="header-user-name"> </h4> : <h4 className="header-user-name"> Hi, {this.firstname()} </h4>}
-					<div className="dropdown">
-						<div className="header-photo" onClick={this.clickToshowBtn.bind(this)}>
-						{this.state.details.username == "" || this.state.details.username == null ? 
-						<span></span> :	<img src={this.state.details.photo} alt={this.state.details.photo} />
-						}
-						</div>
-						<div className="dropdown-content">
-							<FacebookLoginButton fb={FB} userInfo={this.userInfo}/>
-						</div>
-					</div>
-				</div>
-			)
+	componentWillMount() {
+		
+		window.onclick = (event) => {
+			if(!(this.state.details.username == "" || this.state.details.username == null)){
+	  			if (!event.target.matches('.img-clk-drpdown')) {
+	  				$(".dropdown-content").css({
+	  					'display' : 'none'
+	  				})
+	  			}
+	  		}
+		}
 		
 	}
+
+
+
+	sellerLogoutAction = () =>{
+		Auth.deauthenticateUser()
+		localStorage.removeItem('userType')
+		this.setState({
+			details : {
+				userType : "",
+				username: null,
+				email: null,
+				age: null,
+				photo: null	
+			}
+		})
+		location.reload()
+
+	}
+
+
+	render() {
+
+		console.log("the value of state userType" + this.state.details.userType)
+		return(
+			<div className="header">
+				{this.checkForButton()}
+
+				{!Auth.isUserAuthenticated() && (this.state.details.userType == "" || this.state.details.userType == null) ? 
+					<div className="seller-login-info">
+						<Link to={'/Login'}> Click here to login if you are a seller </Link>
+					</div>  : <span> </span>}
+					
+				
+				<h2 className="title-text"> Consumable </h2>
+
+				{(this.state.details.username == "" || this.state.details.username == null) ? 
+				<h4 className="header-user-name"> </h4> : 
+				<h4 className="header-user-name"> Hi, {this.firstname()} </h4>}
+
+				<div className="dropdown">
+					<div className="header-photo" onClick={this.clickToshowBtn.bind(this)}>
+
+						{this.state.details.username == "" || this.state.details.username == null ? 
+						<span></span> :
+						<img src={this.state.details.photo} className="img-clk-drpdown" alt={this.state.details.photo} />
+						}
+
+					</div>
+					<div className="dropdown-content">
+						<FacebookLoginButton fb={FB} userInfo={this.userInfo}/>
+					</div>
+				</div>
+
+				{Auth.isUserAuthenticated() ?
+						<div className="seller-logout">
+							<div className="seller-logout-button" onClick={this.sellerLogoutAction.bind(this)}> Log Out </div>
+						</div> :
+					<span> </span>}
+			</div>
+		)
+		
+	}
+		
 }
